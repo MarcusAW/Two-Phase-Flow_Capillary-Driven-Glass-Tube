0c3208c1909731651572dc3b0f0497c06bece02b
Marcus Ang <maw272727@gmail.com>
2024-05-30 14:14:57 -0500

    Upload

---
+++ parallelAllRun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=validationRun2
+#SBATCH --time=40:00:00
+#SBATCH --ntasks=36
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=2G
+
+module load OpenFOAM/v2106-foss-2021a
+. ${FOAM_BASH}
+
+decomposePar
+mpirun --mca btl ^openib -np 36 renumberMesh -overwrite -parallel
+mpirun --mca btl ^openib -np 36 interFlow -parallel
+
+
